869439e2f8d8895243dc8fd4cbf0cf75f11ad563
gracebir <grace.devolop@gmail.com>
2023-06-30 13:06:44 +0200

    input text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import {MdSend} from 'react-icons/md'
 
 function App() {
   return (
-    <div className='h-screen font-inter bg-[#282c34]'>
+    <div className='h-screen font-inter bg-[#282c34] flex flex-col lg:flex-row'>
       <Sidebar />
-      <div>
-
+      <div className='flex-1 w-full px-2'>
+        <div className="flex flex-col justify-between h-full max-w-3xl mx-auto lg:py-8 py-0">
+          <div className='flex overflow-y-scroll scroll-hidden h-[84.5vh] lg:h-[86vh] flex-col py-2 text-white'>
+          </div>
+          <div className='py-3 px-4 w-full bg-[#363c46] rounded-xl flex items-center mb-4 lg:mb-0'>
+            <input className='w-full px-4 bg-[#363c46] py-2 outline-none rounded' placeholder='Send a Message' type="text" name="" id="" />
+            <span className='text-gray-400 cursor-pointer'>
+              <MdSend size={25}/>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
