62124878fbc7dfa18c90cc5b0bab037db1ae5f02
gracebir <grace.devolop@gmail.com>
2023-06-30 15:50:33 +0200

    handle input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import {MdSend} from 'react-icons/md'
 import Chat from './components/Chat'
 
 function App() {
+  const [message, setMessage] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("hey, submit")
+  }
   return (
     <div className='h-screen font-inter bg-[#282c34] flex flex-col lg:flex-row'>
       <Sidebar />
@@ -13,12 +18,12 @@ function App() {
             <Chat isAi={false} user={"Grace"} text={"How are you?"}/>
             <Chat isAi={true} user={"AI"} text={"Yes, I am going great, how about you?"}/>
           </div>
-          <div className='py-3 px-4 w-full max-w-3xl mx-auto bg-[#363c46] shadow-lg rounded-xl flex items-center mb-4 lg:mb-0'>
-            <input className='w-full px-4 bg-[#363c46] py-2 outline-none rounded' placeholder='Send a Message' type="text" name="" id="" />
-            <span className='text-gray-400 cursor-pointer'>
+          <form onSubmit={handleSubmit} className='py-3 px-4 w-full max-w-3xl mx-auto bg-[#363c46] shadow-lg rounded-xl flex items-center mb-4 lg:mb-0'>
+            <input onChange={(e)=> setMessage(e.target.value)} className='w-full text-gray-200 px-4 bg-[#363c46] py-2 outline-none rounded' placeholder='Send a Message' type="text" name="" id="" />
+            <button className='text-gray-400 cursor-pointer'>
               <MdSend size={25}/>
-            </span>
-          </div>
+            </button>
+          </form>
         </div>
       </div>
     </div>
