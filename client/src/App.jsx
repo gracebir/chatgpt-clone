62c8d76014fab2d7bcf97186c616127e9e4c4162
gracebir <grace.devolop@gmail.com>
2023-06-30 13:57:19 +0200

    almost done with the message contain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <div className="flex flex-col justify-between h-full max-w-3xl mx-auto lg:py-8 py-0">
           <div className='flex overflow-y-scroll scroll-hidden h-[84.5vh] lg:h-[86vh] flex-col py-2 text-white'>
           </div>
-          <div className='py-3 px-4 w-full bg-[#363c46] rounded-xl flex items-center mb-4 lg:mb-0'>
+          <div className='py-3 px-4 w-full bg-[#363c46] shadow-lg rounded-xl flex items-center mb-4 lg:mb-0'>
             <input className='w-full px-4 bg-[#363c46] py-2 outline-none rounded' placeholder='Send a Message' type="text" name="" id="" />
             <span className='text-gray-400 cursor-pointer'>
               <MdSend size={25}/>
